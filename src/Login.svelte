5f8fc1349a1fd6e10d35fc9a8b395cf068de92f9
Eric Sommerhalder <eric@Erics-iMac.local>
2021-11-28 22:57:35 +0100

    Added Login component

---
+++ src/Login.svelte
@@ -0,0 +1,10 @@
+<script>
+    let user, password;
+    function login(user, password){
+        console.log("User: " + user)
+        console.log("Passwort: " + password)
+    }
+</script>
+<input bind:value={user} placeholder="Benutzer"/>
+<input type:password bind:value={password} placeholder="Passwort"/>
+<a on:click={login(user, password)}>LOGIN</a>
